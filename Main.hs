603c63b735a11e2f95fd50ded247146a5bde2bd2
Sergey Gerasimov <mail@grsmv.com>
2014-11-01 23:53:40 +0200

    upd

--- Main.hs
+++ Main.hs
@@ -2,18 +2,20 @@
 
 module Main where
 
-import Control.Concurrent (forkIO, threadDelay)
-import Control.Monad (forever)
-import Control.Monad.IO.Class (liftIO)
-import Data.Aeson
-import Data.IORef (newIORef, readIORef, writeIORef)
-import Data.Time
-import Data.Time.Format
-import Data.Tree.NTree.TypeDefs
-import System.Locale
-import Text.HandsomeSoup
-import Text.XML.HXT.Core
-import Web.Scotty
+import           Control.Concurrent       (forkIO, threadDelay)
+import           Control.Monad            (forever)
+import           Control.Monad.IO.Class   (liftIO)
+import           Data.Aeson
+import           Data.IORef               (newIORef, readIORef, writeIORef)
+import           Data.Maybe               (fromMaybe)
+import           Data.Time
+import           Data.Time.Format
+import           Data.Tree.NTree.TypeDefs
+import           System.Locale
+import           System.Posix.Env         (getEnv)
+import           Text.HandsomeSoup
+import           Text.XML.HXT.Core
+import           Web.Scotty
 
 data Rate = Rate UTCTime Float
 data Currency = EUR | USD
@@ -37,19 +39,19 @@ getRates :: Currency -> IO [Rate]
 getRates currency = do
     -- отримання та парсинг сторінки з курсами нацбанку
     url <- compileUrl currency
-    numbers <- runX $ (fromUrl url) >>> css "table#results0 td.cell_c" 
+    numbers <- runX $ fromUrl url >>> css "table#results0 td.cell_c"
     return $ toRates (map clean numbers)
     where
         -- екcтрактимо внутрішній текст тега
-        clean d' = d where NTree _ ((NTree (XText d) _):_) = d'
+        clean d' = d where NTree _ (NTree (XText d) _:_) = d'
 
         -- перетворюємо одномірний масив на массив курсів валют
-        toRates xs = [ rate (take 4 xs) ] ++ rest
+        toRates xs = rate (take 4 xs) : rest
             where
                 rate (a:_:_:d:[]) = Rate (parse a) (read d :: Float)
                     where parse t = readTime defaultTimeLocale "%d.%m.%Y" t :: UTCTime
                 rest = if length dropped >= 4 then toRates dropped else []
-                    where dropped = drop 4 xs 
+                    where dropped = drop 4 xs
 
 
 -- форматування адреси для забору інформації для обраної валюти за останні 90 діб
@@ -57,10 +59,10 @@ compileUrl :: Currency -> IO String
 compileUrl currency = do
     currentDay <- fmap utctDay getCurrentTime
     let formatDay = formatTime defaultTimeLocale "%d.%m.%Y"
-        today = formatDay currentDay
-        past = formatDay $ addDays (-90) currentDay
-        code = case currency of EUR -> 196
-                                USD -> 169
+        today     = formatDay currentDay
+        past      = formatDay $ addDays (-90) currentDay
+        code      = case currency of EUR -> 196
+                                     USD -> 169
     return $ filter (/='"') "http://www.bank.gov.ua/control/uk/curmetal/currency/search?" ++
       "formType=searchPeriodForm&time_step=daily&outer=table&" ++
       "periodStartTime=" ++ past ++ "&periodEndTime=" ++ today ++ "&" ++
@@ -69,6 +71,7 @@ compileUrl currency = do
 
 main :: IO ()
 main = do
+
     -- початковий збір даних
     exchangeRef <- fetchAndCombineExchangeRates >>= newIORef
 
@@ -77,7 +80,9 @@ main = do
         threadDelay (1000000 * 24 * 60 * 60)
         fetchAndCombineExchangeRates >>= writeIORef exchangeRef
 
-    scotty 3001 $
+    port <- fmap (fromMaybe "3001") (getEnv "PORT")
+
+    scotty (read port :: Int) $
         get "/" $ do
             setHeader "Access-Control-Allow-Origin" "*"
             setHeader "Content-Type" "application/json; charset=utf-8"
